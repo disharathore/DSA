a239823117732c97cb70d18c754fabba82489a67
Disha Rathore <100022221+disharathore@users.noreply.github.com>
2026-02-15 12:36:26 +0530

    Time: 1 ms (99.65%) | Memory: 43.8 MB (38.26%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int n1 = a.length(), n2 = b.length();
+        int max = Math.max(n1, n2);
+        int C = 0, i = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i < max || C > 0) {
+            int A = 0;
+            int B = 0;
+
+            if (i < n1)
+                A = a.charAt(n1 - 1 - i) - '0';
+            if (i < n2)
+                B = b.charAt(n2 - 1 - i) - '0';
+
+            int S = (A ^ B) ^ C;
+            int C_out = ((A ^ B) & C) | (A & B);
+
+            sb.append((char)(S + '0'));
+            C = C_out;
+
+            i++;
+        }
+
+        return sb.reverse().toString();
+    }
+}
